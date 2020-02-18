d49557ab53d5e98407bea2bbe67daed0c672d337
pokkagrinti <60540228+pokkagrinti@users.noreply.github.com>
2020-02-18 16:01:45 +0800

    Add files via upload

--- ParserChecker.py
+++ ParserChecker.py
@@ -5,33 +5,32 @@ import numpy as np
 
 def parser(logfile): #Ensure that access log is in the same directory before runningc
     """This is a function that parses the log file and saves the relevant information to a CSV file"""
-    with open('output.csv', 'w') as csvfile:
-        joinedlist = list()
-        #Use Regex to get the information needed line by line
-        with open(logfile + '.log', 'r') as log_file:
-            for line in log_file:
-                ipregex = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-                datasizenumregex = r'(?<=\d ).\d{1,5}(?= ")'
-                statusregex = r'(?<=" ).\d{1,3}(?= )'
-                datetimeregex = r'\d{2}/\w\w\w/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}'
-                wordsregex = r'(?<= ")(.*)(?= HTTP/)'
+    joinedlist = list()
+    #Use Regex to get the information needed line by line
+    with open(logfile + '.log', 'r') as log_file:
+        for line in log_file:
+            ipregex = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+            datasizenumregex = r'(?<=\d ).\d{1,5}(?= ")'
+            statusregex = r'(?<=" ).\d{1,3}(?= )'
+            datetimeregex = r'\d{2}/\w\w\w/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}'
+            wordsregex = r'(?<= ")(.*)(?= HTTP/)'
 
-                ip = re.findall(ipregex, line)
-                status = re.findall(statusregex, line)
-                datasize = re.findall(datasizenumregex, line)
-                datetime = re.findall(datetimeregex, line)
-                url = re.findall(wordsregex, line)
+            ip = re.findall(ipregex, line)
+            status = re.findall(statusregex, line)
+            datasize = re.findall(datasizenumregex, line)
+            datetime = re.findall(datetimeregex, line)
+            url = re.findall(wordsregex, line)
 
-                #Check if the Data size is empty
-                if len(datasize) == 0:
-                    #Leave column blank if Data size is empty
-                    datasize.append('')
+            #Check if the Data size is empty
+            if len(datasize) == 0:
+                #Leave column blank if Data size is empty
+                datasize.append('')
 
-                #Join the outputs of Regex into one list to use in dataframe
-                joinedlist.append(ip + datasize + datetime + status + url)
+            #Join the outputs of Regex into one list to use in dataframe
+            joinedlist.append(ip + datasize + datetime + status + url)
 
-        df = pd.DataFrame(joinedlist, columns=['IP', 'Data Size', 'Date Time', 'Status', 'URL'])
-        return df
+    df = pd.DataFrame(joinedlist, columns=['IP', 'Data Size', 'Date Time', 'Status', 'URL'])
+    return df
 
 def attackchecker(df, dict):
     '''This function searches the csvfile for any columns that contain the specified string and output them'''
@@ -61,7 +60,7 @@ def attackchecker(df, dict):
     print 'Results: %d Attacks Successful and %d Attempted Attacks redirected' %(successfulcode.shape[0],redirectedcode.shape[0])
 
     # Export to CSV
-    export.to_csv(r'export_dataframe.csv', index=None, header=Trute)
+    export.to_csv(r'export_dataframe.csv', index=None, header=True)
 
 mydict = {'xss':1, 'sql':0, 'rfi':0, 'lfi':0}
-attackchecker(parser('access copy'), mydict)
+attackchecker(parser('accesssmall'), mydict)
