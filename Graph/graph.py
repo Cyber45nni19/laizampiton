ac450998c48bed6abb26bc5e68429892e49afb95
Cyber45nni19 <59224272+Cyber45nni19@users.noreply.github.com>
2020-02-20 14:16:43 +0800

    Update graph.py

--- Graph/graph.py
+++ Graph/graph.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
-from matplotlib import style
 import pandas as pd
-import numpy as np
-import csv
+from matplotlib import style
 
 style.use('ggplot')
 data = pd.read_csv('Example.csv', delimiter=',')
 df = pd.DataFrame(data)
-x = df['NameOfAttack']
-y = df['NumberofTimes']
-
+x = df['IP']
+y = df['Status']
 
 #resize graph
 plt.figure(figsize=(15,6.5), dpi=100)
@@ -21,8 +18,8 @@ plt.bar(x, y, label='Successful', color='blue')
 plt.title('Data Analze', fontdict={'fontname': 'comic Sans MS', 'fontsize': 20})
 
 #Adding label to the graph on the X axis and Y axis
-plt.xlabel('Attacks')
-plt.ylabel('Num of attack (Successful)')
+plt.xlabel('Flagged IP')
+plt.ylabel('Connection (Successful)')
 
 # X and Y axis Tickmarks (scale of your graph)
 plt.yticks([0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300])
